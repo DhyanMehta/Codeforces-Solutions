1064e9389590ad6a9b9a10742fe56e83e072c764
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-06-26 14:04:22 +0530

    add Race

---
+++ A_Race.cpp
@@ -0,0 +1,38 @@
+ 
+/**
+ *    author:  Dhyan_Mehta
+ *    created: 26.06.2025 13:43:01
+**/
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+#define endl '\n'
+#define DHYAN ios::sync_with_stdio(false),cin.tie(nullptr)
+
+
+int main()
+{
+DHYAN;
+
+int t;
+cin>>t;
+
+while(t--)
+{
+int a,x,y;
+cin>>a>>x>>y;
+if(a>x==a>y){
+    cout<<"YES"<<endl;
+}
+else cout<<"NO"<<endl;
+
+
+
+
+
+}
+}
