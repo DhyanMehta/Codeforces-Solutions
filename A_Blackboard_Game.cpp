e08ea1fa5c1d05c63c74d56d893815bda0cc7cf7
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-07-03 22:45:03 +0530

    add Solution

---
+++ A_Blackboard_Game.cpp
@@ -0,0 +1,38 @@
+ 
+/**
+ *    author:  Dhyan_Mehta
+ *    created: 03.07.2025 22:25:13
+**/
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+#define endl '\n'
+#define DHYAN ios::sync_with_stdio(false),cin.tie(nullptr)
+
+
+int main()
+{
+DHYAN;
+
+int t;
+cin>>t;
+
+while(t--)
+{
+
+int n;
+cin>>n;
+if (n % 4 == 0) {
+cout << "Bob"<<endl;
+} else {
+cout << "Alice"<<endl;
+}
+
+
+
+}
+}
---
+++ A_Santa_Claus_and_Candies.cpp
@@ -0,0 +1,48 @@
+ 
+/**
+ *    author:  Dhyan_Mehta
+ *    created: 03.07.2025 00:21:52
+**/
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+#define endl '\n'
+#define DHYAN ios::sync_with_stdio(false),cin.tie(nullptr)
+
+
+int main()
+{
+DHYAN;
+
+// int t;
+// cin>>t;
+
+// while(t--)
+// {
+
+
+
+
+
+// }
+
+
+int n;
+cin>>n;
+vector<int> candies;
+    int sum = 0;
+    int i=1;
+    int children=0;
+    while(sum+i<n){
+        sum+=i;
+        i++;
+        arr[i]=sum;
+        child++;
+    }
+    int remaining=n-sum;
+    
+}
