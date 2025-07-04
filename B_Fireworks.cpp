966efd81b026c9526fdcd1b021b24b5ea34028a3
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-07-04 10:35:06 +0530

    Problem Rating 900

---
+++ B_Fireworks.cpp
@@ -0,0 +1,33 @@
+ 
+/**
+ *    author:  Dhyan_Mehta
+ *    created: 04.07.2025 10:23:04
+**/
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+#define endl '\n'
+#define DHYAN ios::sync_with_stdio(false),cin.tie(nullptr)
+
+
+int main()
+{
+DHYAN;
+
+int t;
+cin>>t;
+
+while(t--)
+{
+
+    ll a,b,m;
+    cin>>a>>b>>m;
+    cout<<(m/a+m/b+2)<<endl;
+
+
+}
+}
