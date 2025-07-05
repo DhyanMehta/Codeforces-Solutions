26273fd1f5d7039193922b8cbfc209be0c289354
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-07-05 22:50:41 +0530

    Problem Rating 800

---
+++ A_Plus_or_Minus.cpp
@@ -0,0 +1,37 @@
+ 
+/**
+ *    author:  Dhyan_Mehta
+ *    created: 05.07.2025 22:29:50
+**/
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+#define endl '\n'
+#define DHYAN ios::sync_with_stdio(false),cin.tie(nullptr)
+
+
+int main()
+{
+DHYAN;
+
+int t;
+cin>>t;
+
+while(t--)
+{
+
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b==c){
+        cout<<"+"<<endl;
+    }
+    else cout<<"-"<<endl;
+
+
+
+}
+}
