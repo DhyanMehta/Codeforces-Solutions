494af62e0d43c9d313af69b6344ede790cf59dda
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-07-14 23:22:35 +0530

    Problem Rating 800

---
+++ A_A_B_Again.cpp
@@ -0,0 +1,36 @@
+ 
+/**
+ *    author:  Dhyan_Mehta
+ *    created: 14.07.2025 23:19:45
+**/
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+#define endl '\n'
+#define DHYAN ios::sync_with_stdio(false),cin.tie(nullptr)
+
+
+int main()
+{
+DHYAN;
+
+int t;
+cin>>t;
+
+while(t--)
+{
+    int n;
+    cin>>n;
+    int a=n/10;
+    int b=n%10;
+    cout<<a+b<<endl;
+
+
+
+
+}
+}
