710b7ab63b0172736eaf9efe49c473628d7850ff
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-07-17 14:54:12 +0530

    Problem Rating 800

---
+++ B_Three_Brothers.cpp
@@ -0,0 +1,31 @@
+
+/**
+ *    author:  Dhyan_Mehta
+ *    created: 17.07.2025 14:48:55
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+#define endl '\n'
+#define DHYAN ios::sync_with_stdio(false), cin.tie(nullptr)
+
+int main()
+{
+    DHYAN;
+
+    // int t;
+    // cin>>t;
+
+    // while(t--)
+    // {
+
+    // }
+
+    int a, b;
+    cin >> a >> b;
+    int sum = 6;
+    cout << sum - (a + b) << endl;
+}
